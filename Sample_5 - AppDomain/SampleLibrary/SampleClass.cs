f17f8674c4299902389ebab2ba0c822192c25b51
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2025-01-29 16:09:16 +0700

    Add files via upload

---
+++ Sample_5 - AppDomain/SampleLibrary/SampleClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Делаем сборку библиотеки 
+namespace SampleLibrary
+{
+    public static class SampleClass
+    {
+        public static void DoSome()
+        {
+            Console.WriteLine("DoSome is Work!");
+        }
+    }
+}
